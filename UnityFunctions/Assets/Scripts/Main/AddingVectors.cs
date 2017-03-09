b13ac44204cd951db8f6640a95149f593e209e89
Vladimir Bodurov <vladimir@bodurov.com>
2017-03-09 15:36:29 -0800

    adding vectors

--- UnityFunctions/Assets/Scripts/Main/AddingVectors.cs
+++ UnityFunctions/Assets/Scripts/Main/AddingVectors.cs
@@ -25,9 +25,10 @@ namespace Main
             _bFrom = fun.meshes.CreateSphere(sphe).SetName("B_From").SetStandardShaderTransparentColor(0, 0, 1, 1).transform;
             _bTo = fun.meshes.CreateCone(cone).SetName("B_To").SetStandardShaderTransparentColor(0, 0, 1, 1).transform;
 
-            _cFrom = fun.meshes.CreateSphere(new DtSphere {radius = sphereRadius/3f}).SetName("C_From").SetStandardShaderTransparentColor(1, 0, 1, 1).transform;
+            _cFrom = fun.meshes.CreateSphere(new DtSphere {radius = sphereRadius}).SetName("C_From").SetStandardShaderTransparentColor(1, 0, 1, 1).transform;
             _cTo = fun.meshes.CreateCone(new DtCone {height = arrowHeight/3f, topRadius = 0.001, bottomRadius = arrowConeBase/3f, localPos = new Vector3(0,-arrowHeight/3f, 0) }).SetName("C_To").SetStandardShaderTransparentColor(1, 0, 1, 1).transform;
 
+            _cFrom.position = V3(0, 0.5, 0);
             _aFrom.position = V3(0, 0, 0);
             _bFrom.position = V3(1, 0, 0);
             _aTo.position = V3(0.4, 0, 0);
@@ -37,17 +38,16 @@ namespace Main
         {
             var vecA = _aTo.position - _aFrom.position;
             var vecB = _bTo.position - _bFrom.position;
-            
+            var cFrom = _cFrom.position;
+
             _aTo.up = vecA.normalized;
             _bTo.up = vecB.normalized;
             Debug.DrawLine(_aFrom.position, _aTo.position, new Color(0, 1, 0, 1),0,true);
             Debug.DrawLine(_bFrom.position, _bTo.position, new Color(0, 0, 1, 1),0,true);
 
             var vec_A_plus_B = vecA + vecB; // B relative to A
-            var cFrom = Vector3.Lerp(_aTo.position, _bTo.position, 0.5f);
             var cTo = cFrom + vec_A_plus_B;
             Debug.DrawLine(cFrom, cTo, new Color(1, 0, 1, 1),0,true);
-            _cFrom.position = cFrom;
             _cTo.position = cTo;
             _cTo.up = vec_A_plus_B.normalized;
         }
